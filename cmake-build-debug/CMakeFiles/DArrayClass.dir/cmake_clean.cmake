ee8722fa8fd5870a156c0702119d3a40aaa50cce
guidoasbun <guido@asbun.io>
2021-09-25 13:52:21 -0700

    Blank initial project creation

---
+++ cmake-build-debug/CMakeFiles/DArrayClass.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DArrayClass.dir/main.cpp.o"
+  "DArrayClass"
+  "DArrayClass.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DArrayClass.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
